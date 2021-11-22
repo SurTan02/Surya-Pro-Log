73e903621e4c44af242895c12ed01e140baf0c3b
farrel-a <13520110@std.stei.itb.ac.id>
2021-11-22 15:18:47 +0700

    Add basic main navigation

--- main.pl
+++ main.pl
@@ -1,5 +1,30 @@
 /* semua include di sini */
 
-:- dynamic(state/1)
+:- include('menu.pl').
 
 
+:- dynamic(state/1).
+
+state(initialized).
+
+start :-
+   mainMenu.
+
+mainMenu :-
+    write('>>> '),
+    read(Choice), nl,
+    ( 
+      (Choice = start, startGame);
+      (Choice = load, loadGame);
+      (Choice = help, help);
+      (Choice = exit, exitGame)
+    ). 
+    
+startGame :- 
+     asserta(state(start)),
+     write('Game Started'),nl.
+     
+loadGame :-
+     write('Game Loaded'),nl.    
+
+exitGame :- halt.
