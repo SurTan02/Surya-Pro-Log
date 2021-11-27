333c669a61b1f227cc1557ce072c5dda07d5e2da
SurTan02 <suryanto.tan5@gmail.com>
2021-11-27 22:26:11 +0700

    nambah main sekadarnya

--- main.pl
+++ main.pl
@@ -1,29 +1,72 @@
 /* semua include di sini */
 
 :- include('menu.pl').
+:- include('peta.pl').
+:- include('player.pl').
+:- include('activity.pl').
+:- include('command.pl').
 
 
 :- dynamic(state/1).
 
 state(initialized).
 
+
 start :-
-   mainMenu.
+     mainMenu.
+     % repeat,
+     % write('>>> '),
+     % read(X),
+     % call(X),
+     % fail.
+     
 
 mainMenu :-
-    write('>>> '),
-    read(Choice), nl,
-    ( 
-      (Choice = start, startGame);
-      (Choice = load, loadGame);
-      (Choice = help, help);
-      (Choice = exit, exitGame)
-    ). 
+     help,
+     write('>>> '),
+     read(Choice), nl,!,
+     ( 
+          (Choice = start, startGame);
+          (Choice = load, loadGame);
+          (Choice = help, help);
+          (Choice = exit, exitGame)
+     ).
     
 startGame :- 
-     asserta(state(start)),
-     write('Game Started'),nl.
-     
+     reset,
+     asserta(state(started)),
+     initMap,
+
+     write('Game Started'),nl,
+     write('Selamat Datang di Game!'),nl,
+     write('Pilih Job yang ingin Anda Pilih!'),nl,
+     write('1. fisherman'),nl,
+     write('2. rancher'),nl,
+     write('3. farmer'),nl,
+     write('>>> '),
+     read(Choice), nl,
+     pilihJob(Choice),
+     menuInGame.
+
+menuInGame :-
+     write('Pilih Hal yang ingin dilakukan!'),nl,
+     write('1. w,a,s,d untuk berpindah tempat'),nl,
+     write('2. map'),nl,
+     write('3. status'),nl,
+     write('>>> '),
+     read(Choice), nl,!,
+     ( 
+          (Choice = w, w);
+          (Choice = a, a);
+          (Choice = s, s);
+          (Choice = d, d);
+          (Choice = map, map);
+          (Choice = status, showStat)
+     ),
+     menuInGame.
+
+
+
 loadGame :-
      write('Game Loaded'),nl.    
 
