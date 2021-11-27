c8a8ce8ae08e2ef3bbc4097e4a5cc3614ac68dbb
nandono206 <79740572+nandono206@users.noreply.github.com>
2021-11-28 02:28:47 +0700

    add inventory, store, and minor update to items

---
+++ inventory.pl
@@ -0,0 +1,90 @@
+:- dynamic(myInventory/9).
+
+% :- include('items.pl').
+% % myInventory(ID, Name, String_Name, Type, Level, EnergyNeed, EnergySupply, Price, Count).
+
+% % FOR DEBUGGING PURPOSES
+% myInventory(30, fishingrod, 'level 2 fishing rod', equipment, 2, 0, 0, 100, 1).
+% myInventory(10, carrot_seed, 'carrot seed', commodity, 1, 350, 0, 5, 10).
+% myInventory(11, potato_seed, 'potato seed', commodity, 1, 375, 0, 7, 2).
+
+
+maxInventory(100).
+
+addToInventory(_) :-
+    cekJumlahInventory(Sum),
+    maxInventory(Max),
+    Sum >= Max,
+    write('Inventory Full'),
+    !,fail.
+
+addToInventory(ID) :-
+    findall(InvID, myInventory(InvID,_,_,_,_,_,_,_,_), ListID),
+    (   isInInventory(ID, ListID) -> %jikka ada di inventory
+        item(ID, Name,_,_,_,_,_,_),
+        retract(myInventory(ID, Name, String_Name, Type, Level, EnergyNeed, EnergySupply, Price, Count)),
+        NewCount is Count+1,
+        assertz(myInventory(ID, Name, String_Name, Type, Level, EnergyNeed, EnergySupply, Price, NewCount))
+    ;   item(ID, Name, String_Name, Type, Level, EnergyNeed, EnergySupply, Price),
+        Count is 1,
+        assertz(myInventory(ID, Name, String_Name, Type, Level, EnergyNeed, EnergySupply, Price, Count))
+    ).
+
+addNtimes(0,_).
+addNtimes(N, ID) :-
+    addToInventory(ID),
+    S is N-1, 
+    addNtimes(S, ID), !.
+
+deleteFromInventory(ID) :-
+    findall(InvID, myInventory(InvID,_,_,_,_,_,_,_,_), ListID),
+    (   isInInventory(ID, ListID) -> %jikka ada di inventory
+        item(ID, Name,_,_,_,_,_,_),
+        myInventory(ID, Name, String_Name, Type, Level, EnergyNeed, EnergySupply, Price, Count),
+        NewCount is Count-1,
+        (
+            NewCount >= 1 ->
+            retract(myInventory(ID, Name, String_Name, Type, Level, EnergyNeed, EnergySupply, Price, Count)),
+            assertz(myInventory(ID, Name, String_Name, Type, Level, EnergyNeed, EnergySupply, Price, NewCount))
+        ;   retract(myInventory(ID, Name, String_Name, Type,    Level, EnergyNeed, EnergySupply, Price, Count))
+
+
+
+        )
+    ;   write('You dont have that item on your inventory')
+    ).      
+
+deleteNtimes(0,_).
+deleteNtimes(N, ID) :-
+    deleteFromInventory(ID),
+    S is N-1, 
+    deleteNtimes(S, ID), !.
+
+cekJumlahInventory(Sum):-
+    findall(Count, myInventory(_,_,_,_,_,_,_,_, Count), List),
+    sum_list(List, Sum).
+
+isInInventory(ID,[ID|_]).
+isInInventory(ID,[_|T]) :- isInInventory(ID,T).
+
+isFull :-
+    cekJumlahInventory(Sum),
+    Sum == 100.
+
+makeListInventory(String_Name_List, CountList) :-
+    findall(String_Name, myInventory(_,_, String_Name,_,_,_,_,_,_), String_Name_List),
+    findall(Count, myInventory(_,_,_,_,_,_,_,_,Count), CountList).
+
+writeInventory([], []).
+writeInventory([A|V], [B|W]) :-
+        write(B), write(' '), write(A), nl,
+        writeInventory(V,W).
+
+
+
+    
+inventory :-
+    cekJumlahInventory(X),
+    write('Your Inventory ('), write(X), write('/100)'), nl,nl,
+    makeListInventory(String_Name_List, CountList),
+    writeInventory(String_Name_List, CountList).
---
+++ market.pl
@@ -0,0 +1,185 @@
+% boolean apakah player didalam store
+:- dynamic(isInMarket/1).
+
+:- include('items.pl').
+:- include('inventory.pl').
+
+% FOR DEBUGGING PURPOSES
+myInventory(30, fishingrod, 'level 2 fishing rod', equipment, 2, 0, 0, 100, 1).
+myInventory(10, carrot_seed, 'carrot seed', commodity, 1, 350, 0, 5, 10).
+myInventory(11, potato_seed, 'potato seed', commodity, 1, 375, 0, 7, 2).
+
+playerCoord(2,2).
+marketCoord(2,2).
+
+% kondisi player diluar berada bukan pada tile market
+market :-
+    playerCoord(X,Y),
+    \+marketCoord(X,Y),
+    write('Go to Market (M) to access this command!'),nl,
+    !,fail.
+
+market :-
+    asserta(isInMarket(1)), repeat,
+    write('Welcome to the Market Claire!'), nl,
+    write('What do you want to do?'), nl,
+    write('1. Buy'), nl,
+    write('2. Sell'), nl,
+    write('3. Exit'), nl,
+    read(X), nl,
+    ((X < 1 ; X > 3) -> 
+        write('Invalid input! choose 1 or 2'), nl, fail
+    ; (X =:= 1) ->
+        write('What do you want to buy?'), nl,
+        write('1. carrot seed (5 gold)'), nl,
+        write('2. potato seed (7 gold)'), nl,
+        write('3. corn seed (12 gold) *need lvl 2 shovel to plant'), nl,
+        write('4. pumpkin seed (15 gold) *need lvl 3 shovel to plant'), nl,
+        write('5. chicken (500 gold)'), nl,
+        write('6. cow (2000 gold)'), nl,
+        write('7. sheep (1200 gold)'), nl, nl,
+        write('**EQUIPMENTS**'), nl,
+        writeShovel,
+        writeRod,
+        writeBoat,
+        write('Write the name of the item that you want to buy'), nl,
+        write('example: \'carrot seed\', to buy carrot seed. '), nl,
+        read(ItemName), nl,
+        write('How much did you want to buy?'), nl,
+        read(AmountItem), nl, 
+        buy(ItemName, AmountItem), nl, fail
+    ; (X =:= 2) -> 
+        inventory, nl,
+        write('Write the name of the item that you want to buy'), nl,
+        write('example: \'carrot seed\', to sell carrot seed. '), nl,
+        read(StringName),
+        findall(InvStrName, myInventory(_,_, InvStrName,_,_,_,_,_,_), String_Name_List),
+        (
+            isInInventory(StringName, String_Name_List) ->
+            write('How much of that item you want to sell?'), nl,
+            read(N),
+            sell(StringName, N), nl, fail
+        
+        ;   write('You don\'t have that item in your inventory'), nl, fail
+        )
+    ;  write('Terimakasih sudah mengunjungi Store! :D'),nl,
+        retract(isStore(_)),!
+    ). 
+
+writeShovel :-
+    findall(InvID, myInventory(InvID,_, _,_,_,_,_,_,_), ListID),
+    isInInventory(28, ListID),
+    write('- Shovel at MAX Level!'), nl.
+writeShovel :-
+    findall(InvID, myInventory(InvID,_, _,_,_,_,_,_,_), ListID),
+    isInInventory(27, ListID),
+    write('- Level 3 shovel (150 Golds)'), nl.
+writeShovel :-
+    findall(InvID, myInventory(InvID,_, _,_,_,_,_,_,_), ListID),
+    isInInventory(26, ListID),
+    write('- Level 2 shovel (100 Golds)'), nl.
+writeShovel :-
+    findall(InvID, myInventory(InvID,_, _,_,_,_,_,_,_), ListID),
+    \+isInInventory(26, ListID),
+    write('- Level 1 shovel (50 Golds)'), nl.
+
+
+writeRod :-
+    findall(InvID, myInventory(InvID,_, _,_,_,_,_,_,_), ListID),
+    isInInventory(31, ListID),
+    write('- Fishing Rod is at MAX Level'), nl.
+writeRod :-
+    findall(InvID, myInventory(InvID,_, _,_,_,_,_,_,_), ListID),
+    isInInventory(30, ListID),
+    write('- Level 3 fishing rod (150 Golds)'), nl.
+writeRod :-
+    findall(InvID, myInventory(InvID,_, _,_,_,_,_,_,_), ListID),
+    isInInventory(29, ListID),
+    write('- Level 2 fishing rod (100 Golds)'), nl.
+writeRod :-
+    findall(InvID, myInventory(InvID,_, _,_,_,_,_,_,_), ListID),
+    \+isInInventory(29, ListID),
+    write('- Level 1 fishing rod (50 Golds)'), nl.
+
+
+
+
+
+
+
+writeBoat :-
+    findall(InvID, myInventory(InvID,_, _,_,_,_,_,_,_), ListID),
+    isInInventory(34, ListID),
+    write('- Boat is at MAX Level'), nl.
+writeBoat :-
+    findall(InvID, myInventory(InvID,_, _,_,_,_,_,_,_), ListID),
+    isInInventory(33, ListID),
+    write('- Level 3 boat (300 Golds)'), nl.
+writeBoat :-
+    findall(InvID, myInventory(InvID,_, _,_,_,_,_,_,_), ListID),
+    isInInventory(32, ListID),
+    write('- Level 2 boat (300 Golds)'), nl.
+writeBoat :-
+    findall(InvID, myInventory(InvID,_, _,_,_,_,_,_,_), ListID),
+    \+isInInventory(32, ListID),
+    write('- Level 1 boat (200 Golds)'), nl.
+
+
+
+
+buy(_,_) :-
+    \+isInMarket(_),
+    write('Illegal command!'),nl,
+    write('Go to the nearest store to buy items.'),nl,!,fail.
+
+buy(StringName,N) :-
+    isInMarket(_),
+    money(Money),
+    item(_, _, StringName,_,_,_,_, Price),
+    TotalPrice is Price * N,
+    Money < TotalPrice,
+    write('Insufficient fund!'), write(StringName), nl,
+    write('Ganbatte!'),nl,!,fail.
+
+buy(_,N) :-
+    isInMarket(_),
+    cekJumlahInventory(X),
+    AddEl is X + N,
+    AddEl > 100,
+    write('Your inventory is full!'),nl,
+    write('sell your item to make more space!'),nl,!,fail.
+
+buy(StringName, N) :-
+    isInMarket(_),
+    item(ID,_,StringName,_,_,_,_,Price),
+    addNtimes(N, ID),
+    TotalPrice is Price * N, 
+    money(Money),
+    NowMoney is Money-TotalPrice,
+    write(StringName), write(' succesfully purchased'), nl, write(N), write('x '),
+    write(StringName), write(' is succesfully added to your inventory'),nl,
+    write('Current Money: '), write(NowMoney), write(' gold.'),nl,!.
+
+sell(_,_) :-
+    \+isInMarket(_),
+    write('Invalid Command!'),nl,
+    write('Go to the nearest marketplace (M) to sell item!'),nl,!,fail.
+
+sell(StringName,_) :-
+    isInMarket(_),
+    myInventory(ID,_, StringName,_,_,_,_,_,_),
+    findall(InvID, myInventory(InvID,_, StringName,_,_,_,_,_,_), ListID),
+    \+isInInventory(ID, ListID),
+    write('You dont have that item in your inventory!'), nl,
+    !, fail.
+
+sell(StringName, N) :-
+    isInMarket(_),
+    myInventory(ID,_, StringName,_,_,_,_,Price,_),
+
+    deleteNtimes(N,ID),
+    money(Money),
+    TotalPrice is N*Price,
+    NowMoney is Money + TotalPrice,
+    write('You sold '), write(N), write(' '), write(StringName), nl,
+    write('You received '), write(NowMoney), write(' gold.'),nl,!.
